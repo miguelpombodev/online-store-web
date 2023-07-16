c95ebdaf382bb7f9625b6ca05e229da729cd3ff0
Miguel Pombo <miguel.pomboshape@modec.com>
2023-07-16 19:36:34 -0300

    feat: added plopjs

---
+++ generators/plopfile.js
@@ -0,0 +1,25 @@
+export default function (plop) {
+  // component generator
+  plop.setGenerator("component", {
+    description: "application component logic",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "component name please",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/index.tsx",
+        templateFile: "templates/index.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/styles.tsx",
+        templateFile: "templates/styles.ts.hbs",
+      },
+    ],
+  });
+}
